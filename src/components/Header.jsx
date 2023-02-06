577e41e5f54e253ed524c2480cdd466719cb53ef
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2023-02-06 22:13:51 +0530

    Working on the hero text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,33 @@ import  AppBar from '@mui/material/AppBar'
 import {Typography} from '@mui/material'
 import {IconButton,Toolbar } from '@mui/material'
 import SortIcon from '@mui/icons-material/Sort';
+import { Box } from '@mui/system';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const Header = () => {
   return (
     <div>
-      <AppBar  position='static' sx={{ background:'none',elevation: '0'}}>
+      <AppBar  position='static' sx={{ background:'none',elevation: '0',fontFamily:'Montserrat',border:0 }}>
         <Toolbar width='80%' margin='0 auto'>
-        <Typography variant='h4' sx={{ flexGrow:'1'}}>MY ANIME WEBSITE</Typography>
+        <Typography variant='h4' sx={{ flexGrow:'1',fontFamily:'Montserrat'}}>ANIME-MX</Typography>
         <IconButton edge='start' color='blue' arial-label='menu' sx={{mr:2}}>
             <SortIcon sx={{ color:'#fff',fontSize:'2rem'}} />
         </IconButton>
         </Toolbar>
       </AppBar>
+      <Box display='block' p={2} m={2}>
+      <Typography variant='h1'  height='100vh' fontFamily='Montserrat' fontSize='50px'  sx={{ display:'flex',
+      justifyContent:'center',
+      alignItems:'center' ,
+      fontSize:'3rem',
+      color:'#fff',
+      textAlign:'center',
+      p:2,m:2}} gutterBottom>WELCOME TO MY{ ' '}ANIME WEBSITE
+      </Typography>
+      <IconButton>
+        <ExpandMoreIcon fontSize='2rem' />
+      </IconButton>
+      </Box>
     </div>
   )
 }
