88fd4736525bc111507bf2383653df300f22af7a
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2023-02-07 12:01:39 +0530

    Working on the hero text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,27 +8,53 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const Header = () => {
   return (
-    <div>
+    <div padding='20px' margin='20px'>
       <AppBar  position='static' sx={{ background:'none',elevation: '0',fontFamily:'Montserrat',border:0 }}>
         <Toolbar width='80%' margin='0 auto'>
-        <Typography variant='h4' sx={{ flexGrow:'1',fontFamily:'Montserrat'}}>ANIME-MX</Typography>
-        <IconButton edge='start' color='blue' arial-label='menu' sx={{mr:2}}>
-            <SortIcon sx={{ color:'#fff',fontSize:'2rem'}} />
+        <Typography variant='h4'
+        height='20%'
+         sx={{ flexGrow:'1',
+        fontFamily:'Montserrat'}}>ANIME-MX
+        </Typography>
+        <IconButton edge='start'
+        color='blue'
+        arial-label='menu'
+        sx={{mr:2}}>
+            <SortIcon sx={{ color:'#fff',
+            fontSize:'2rem'}} />
         </IconButton>
         </Toolbar>
       </AppBar>
-      <Box display='block' p={2} m={2}>
-      <Typography variant='h1'  height='100vh' fontFamily='Montserrat' fontSize='50px'  sx={{ display:'flex',
+      <Box display='flex' p='20px' m='20px'>
+      <Typography variant='h1'
+      height='10'
+      width='20'
+      fontFamily='Montserrat'
+      fontSize='50px'
+      sx={{ display:'flex',
       justifyContent:'center',
       alignItems:'center' ,
       fontSize:'3rem',
       color:'#fff',
       textAlign:'center',
       p:2,m:2}} gutterBottom>WELCOME TO MY{ ' '}ANIME WEBSITE
-      </Typography>
       <IconButton>
-        <ExpandMoreIcon fontSize='2rem' />
+        <ExpandMoreIcon
+        fontSize='50px'
+        height = '100vh'
+        sx={{
+          display:'inline-block',
+          justifyContent:'center',
+          alignItems:'center',
+          fontSize:'4rem',
+          color:'#fff',
+          textAlign:'center',
+          p:2 ,
+          m:2
+        }}
+         />
       </IconButton>
+      </Typography>
       </Box>
     </div>
   )
